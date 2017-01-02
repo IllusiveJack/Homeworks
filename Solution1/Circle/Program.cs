c72ce8cc4ec358146644087f81762f3b97da5660
IllusiveJack <kill1234@abv.bg>
2017-01-03 01:22:25 +0200

    Exercise 3

---
+++ Solution1/Circle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main()
+        {
+            double parameter;
+            do
+            {
+                parameter = double.Parse(Console.ReadLine());
+            } while (parameter <= 0);
+
+
+            double area = parameter * 3.14 * parameter;
+            double perimeter = parameter * 2 * 3.14;
+
+            Console.WriteLine(area.ToString("#.00"));
+            Console.WriteLine(perimeter.ToString("#.00"));
+        }
+    }
+}
