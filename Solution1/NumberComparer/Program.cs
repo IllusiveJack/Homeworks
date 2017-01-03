a587a64e90fffe1933e9ab21a38596eec9d211f9
IllusiveJack <kill1234@abv.bg>
2017-01-03 02:23:06 +0200

    Falling Rocks

---
+++ Solution1/NumberComparer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumberComparer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Please enter the first number: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Please enter the second number: ");
+            double b = double.Parse(Console.ReadLine());
+
+            if(a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else if(a < b)
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
