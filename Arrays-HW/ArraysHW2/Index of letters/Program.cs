0f528a88bbdb9c6352fa14c8e3e4bcdd233828f2
Krum Zahariev <kbz150@aubg.edu>
2017-01-12 02:21:17 +0200

    New HW

---
+++ Arrays-HW/ArraysHW2/Index of letters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Index_of_letters
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+
+            char[] arr;
+            arr = str.ToCharArray();
+            char[] alphabet = new char[25];
+
+            for (int i = 0; i < alphabet.Length ; i++)
+            {
+                alphabet[i] = (char)('a' + i);
+            }
+           
+
+            foreach(char letter in arr)
+            {
+                for(int k = 0; k < alphabet.Length; k++)
+                {
+                    if(letter == alphabet[k])
+                    {
+                        Console.WriteLine("The index of '{0}' is {1}", letter, k);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Arrays-HW/ArraysHW2/Merge Sort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the size of the array: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("Enter the value of array #" + (arr[i] + 1) + ": ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(arr);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("{0}, ", arr[i]);
+        }
+    }
+}
